07747779afb0e31d28e4a80c4e696aa0eb9d7a8d
kext <luffyke@gmail.com>
2018-07-26 15:48:52 +0800

    add and set

--- cvChain.go
+++ cvChain.go
@@ -2,28 +2,37 @@ package main
 
 import (
     "fmt"
+    "encoding/json"
 
     "github.com/hyperledger/fabric/core/chaincode/shim"
     "github.com/hyperledger/fabric/protos/peer"
 )
 
-func (t *SimpleAsset) Init(stub shim.ChaincodeStubInterface) peer.Response {
+type CvChain struct {
+}
+
+type CV struct {
+    Company   string `json:"company"`
+	Position  string `json:"position"`
+}
+
+func (t *CvChain) Init(stub shim.ChaincodeStubInterface) peer.Response {
 
 }
 
-func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
-	fn, args := stub.GetFunctionAndParameters()
-	var result string
+func (t *CvChain) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
+    fn, args := stub.GetFunctionAndParameters()
+    var result string
     var err error
     if fn == "addRecord" {
         result, err = add(stub, args)
     } else if fn == "getRecord" {
         result, err = get(stub, args)
     } else if fn == "encRecord" {
-		result, err = enc(stub, args)
-	} else if fn == "decRecord" {
-		result, err = dec(stub, args)
-	}
+        result, err = enc(stub, args)
+    } else if fn == "decRecord" {
+        result, err = dec(stub, args)
+    }
     if err != nil {
         return shim.Error(err.Error())
     }
@@ -31,49 +40,48 @@ func (t *SimpleAsset) Invoke(stub shim.ChaincodeStubInterface) peer.Response {
     return shim.Success([]byte(result))
 }
 
-func set(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+func add(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+    /*
     if len(args) != 2 {
         return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
     }
-
-    err := stub.PutState(args[0], []byte(args[1]))
+    */
+    // key
+    var key = args[0] + "-" + args[1]
+    // value
+    var cv = CV{Company: args[2], Position: args[3]}
+    cvAsBytes, _ := json.Marshal(cv)
+    err := stub.PutState(key, cvAsBytes)
     if err != nil {
-        return "", fmt.Errorf("Failed to set asset: %s", args[0])
+        return "", fmt.Errorf("Failed to set asset: %s", key)
     }
-    return args[1], nil
+    return string(cvAsBytes), nil
 }
 
 // Get returns the value of the specified asset key
 func get(stub shim.ChaincodeStubInterface, args []string) (string, error) {
+    /*
     if len(args) != 1 {
         return "", fmt.Errorf("Incorrect arguments. Expecting a key")
     }
-    value, err := stub.GetState(args[0])
+    */
+    // key
+    var key = args[0] + "-" + args[1]
+    value, err := stub.GetState(key)
     if err != nil {
-        return "", fmt.Errorf("Failed to get asset: %s with error: %s", args[0], err)
+        return "", fmt.Errorf("Failed to get asset: %s with error: %s", key, err)
     }
     if value == nil {
-        return "", fmt.Errorf("Asset not found: %s", args[0])
+        return "", fmt.Errorf("Asset not found: %s", key)
     }
-    return string(value), nil
-}
-
-// Set stores the asset (both key and value) on the ledger. If the key exists,
-// it will override the value with the new one
-func add(stub shim.ChaincodeStubInterface, args []string) (string, error) {
-    if len(args) != 2 {
-       	return "", fmt.Errorf("Incorrect arguments. Expecting a key and a value")
-    }
-    err := stub.PutState(args[0], []byte(args[1]))
-    if err != nil {
-        return "", fmt.Errorf("Failed to set asset: %s", args[0])
-    }
-    return args[1], nil
+    cv := CV{}
+	json.Unmarshal(value, &cv)
+    return cv.Company, nil
 }
 
 // main function starts up the chaincode in the container during instantiate
 func main() {
-    if err := shim.Start(new(SimpleAsset)); err != nil {
-            fmt.Printf("Error starting SimpleAsset chaincode: %s", err)
+    if err := shim.Start(new(CvChain)); err != nil {
+        fmt.Printf("Error starting CvChain chaincode: %s", err)
     }
 }
